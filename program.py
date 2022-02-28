4c5b71ee8695f17b75230cc2454767ec050a88f2
Olga Chernova <99567631+Chocoladov@users.noreply.github.com>
2022-02-28 15:17:57 +1200

    Update program.py

--- program.py
+++ program.py
@@ -1,8 +1,11 @@
-print('Я домашка qqqq')
+print('Я домашка')
 
 git commit -m "My first commit"
+
 # сделали первый коммит
 # текст комментария: "My first commit"
 # комментарии лучше писать латиницей 
+
+print('Я')
 
 
